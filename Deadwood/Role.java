d59b5e3a5b54ead964e0301eae74ac46a80f73ee
Peter Hafner <hafnerp2@wwu.edu>
2023-04-29 13:38:15 -0700

    added code to Role.java

--- Deadwood/Role.java
+++ Deadwood/Role.java
@@ -1,2 +1,38 @@
 public class Role {
+    private String name;
+    private String flavorText;
+    private int rank;
+    private boolean onCard;
+    private boolean taken;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        this.name = n;
+    }
+    public String getFlavorText(){
+        return flavorText;
+    }
+    public void setFlavorText(String f){
+        this.flavorText = f;
+    }
+    public int getRank(){
+        return rank;
+    }
+    public void setRank(int r){
+        this.rank = r;
+    }
+    public boolean isOnCard(){
+        return onCard;
+    }
+    public void setOnCard(boolean b){
+        this.onCard = b;
+    }
+    public boolean isTaken(){
+        return taken;
+    }
+    public void setTaken(boolean t){
+        this.taken = t;
+    }
 }
