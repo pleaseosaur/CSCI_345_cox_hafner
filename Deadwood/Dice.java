e2ec21c8edc2aca06ea797619b82f35a55627bee
pleaseosaur <82362420+pleaseosaur@users.noreply.github.com>
2023-04-29 13:32:17 -0700

    added getters/setters for Dice

--- Deadwood/Dice.java
+++ Deadwood/Dice.java
@@ -1,6 +1,14 @@
 public class Dice {
     private int numDice;
-    private int sides = 6;
+    private int sides;
+
+    public void setSides(int num) {
+        this.sides = num;
+    }
+
+    public int getSides() {
+        return this.sides;
+    }
 
     public int actRoll(){
         // do acting calculations
