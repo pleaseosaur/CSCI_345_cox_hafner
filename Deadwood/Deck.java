65b62216ba04923886889aa7128e628b7e165670
Peter Hafner <hafnerp2@wwu.edu>
2023-04-29 14:07:11 -0700

    added skeleton to Deck.java

--- Deadwood/Deck.java
+++ Deadwood/Deck.java
@@ -1,2 +1,13 @@
 public class Deck {
+    private List<Scene> cards;
+
+    public void setCards(List<Scene> c){
+        this.cards = c;
+    }
+    public void shuffleDeck(){
+        //shuffle logic
+    }
+    public Scene drawScene(){
+        //draw scene logic
+    }
 }
