ad6c3bba627fd210432b78e4156fcb8a064ceb97
pleaseosaur <82362420+pleaseosaur@users.noreply.github.com>
2023-04-29 12:31:54 -0700

    added Deadwood class & began method implementation

---
+++ Deadwood/Deadwood.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Deadwood {
+    private Player player;
+
+    public Deadwood() {
+        setupGame();
+    }
+
+    private void setupGame() {
+        // xml parser here?
+    }
+
+    public void playerMove(String location) {
+        // move logic
+    }
+
+    public void playerUpgrade(int rank) {
+        // upgrade logic
+    }
+
+
+}
--- Deadwood/Main.java
+++ Deadwood/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    public static void main (String[] args) {
+        Deadwood game = new Deadwood();
+        UI ui = new UI(game);
+        ui.run();
+    }
 }
