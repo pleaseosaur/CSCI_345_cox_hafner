84155cc1f84976534c3b4fa7ab881c128b964ae6
Peter Hafner <hafnerp2@wwu.edu>
2023-04-29 14:14:48 -0700

    Added skeleton code for Location and UI

--- Deadwood/Location.java
+++ Deadwood/Location.java
@@ -1,2 +1,26 @@
+import java.security.GeneralSecurityException;
+
 public class Location {
+    private String name;
+    private Scene scene;
+    private List<Location> neighbors;
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String n){
+        this.name = n;
+    }
+    public Scene getScene(){
+        return this.scene;
+    }
+    public void setScene(Scene s){
+        this.scene = s;
+    }
+    public List<Location> getNeighbors(){
+        return neighbors;
+    }
+    public void setNeighbors(List<Location> n){
+        this.neighbors = n;
+    }
 }
--- Deadwood/UI.java
+++ Deadwood/UI.java
@@ -1,2 +1,15 @@
 public class UI {
+    //Scanner to be included here
+    public void playerAction(){
+        //player input here
+    }
+    public void displayMessage(String message){
+        // message logic here
+    }
+    public void displayBoard(Board b){
+        // display board logic here
+    }
+    public void displayStats(Player p){
+        // stat print logic here
+    }
 }
