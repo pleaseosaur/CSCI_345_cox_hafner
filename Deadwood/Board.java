19f23a6abdb3dcc745ca5127ec4c0c67325597ca
Peter Hafner <hafnerp2@wwu.edu>
2023-04-29 14:25:18 -0700

    added skeleton code to Board.java

--- Deadwood/Board.java
+++ Deadwood/Board.java
@@ -1,2 +1,24 @@
 public class Board {
+    private List<Location> locations;
+    private int openScenes;
+
+    public List<Location> getLocations(){
+        return this.locations;
+    }
+    public void setLocations(List<Location> l){
+        this.locations = l;
+    }
+    public int getOpenScenes(){
+        return this.openScenes;
+    }
+    public void setOpenScenes(int o){
+        this.openScenes = 0;
+    }
+
+    public void updatePlayerLocation(Player p, Location l){
+        // do move logic
+    }
+    public boolean endDay(){
+        // end day logic
+    }
 }
