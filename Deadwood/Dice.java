3b6b84739eb143cb0cbb0f79bfbcdf9ac2450741
Peter Hafner <hafnerp2@wwu.edu>
2023-04-29 13:29:10 -0700

    added skeleton methods to Dice and Player

--- Deadwood/Dice.java
+++ Deadwood/Dice.java
@@ -1,13 +1,14 @@
 public class Dice {
     private int numDice;
-    private int sides;
+    private int sides = 6;
 
     public int actRoll(){
+        // do acting calculations
         return -1;
     }
 
     public List<> wrapRoll(){
-        
+        // do wrap calculations
     }
 
 }
--- Deadwood/Player.java
+++ Deadwood/Player.java
@@ -1,2 +1,25 @@
 public class Player {
+    private String name;
+    private int rank;
+    private int credits;
+    private int dollars;
+    private Role role;
+    private int practiceChips;
+    private Location location;
+
+    public void move(){
+        // call method from Deadwood.java to do logic
+    }
+    public void upgrade(){
+        // call method from Deadwood.java to do logic
+    }
+    public void takeRole(){
+        // call method from Deadwood.java to do logic
+    }
+    public void act(){
+        // call method from Deadwood.java to do logic
+    }
+    public void rehearse(){
+        // call method from Deadwood.java to do logic
+    }
 }
