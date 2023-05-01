f29b2e962d738e5d07e33e3e59ab1c01ee97c318
pleaseosaur <82362420+pleaseosaur@users.noreply.github.com>
2023-04-30 17:21:58 -0700

    initial parser setup

--- Deadwood/GameData.java
+++ Deadwood/GameData.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
@@ -14,7 +15,7 @@ import java.io.IOException;
 public class GameData {
 
     public GameData() {
-        // parse xml
+        //empty constructor
     }
 
     // TODO - look into optimizing this repetitive code nonsense
@@ -46,11 +47,23 @@ public class GameData {
         return players;
     }
 
-    public Board createBoard() {
-        // xml parser for board data
-        // data = "dummy data"; //dummy data so return will compile
-        // return Board(data); //dummy return so method compiles
-        return null; //dummy return
+    public Board createBoard()
+    throws ParserConfigurationException {
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = null;
+
+        try {
+            doc = db.parse("board.xml");
+
+
+
+        } catch(Exception e) {
+            System.out.println("XML parse failure");
+            e.printStackTrace();
+        }
+        return doc;
     }
 
     public Deck createDeck() {
