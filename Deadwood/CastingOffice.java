6419fd4c719310c5d7ee743c0b44b24190a3afdc
pleaseosaur <82362420+pleaseosaur@users.noreply.github.com>
2023-05-03 23:00:12 -0700

    minor fix to CastingOffice constructor

--- Deadwood/CastingOffice.java
+++ Deadwood/CastingOffice.java
@@ -3,8 +3,8 @@ import java.util.List;
 public class CastingOffice extends Location{
     private final List<String> upgrades;
 
-    public CastingOffice(String n, Scene s, List<Role> r, List<String> l, List<Take> takes, List<String> upgrades) {
-        super(n, s, r, l, takes);
+    public CastingOffice(String n, Scene s, List<Role> r, List<String> l, List<Take> takes, Area a, List<String> upgrades) {
+        super(n, s, r, l, takes, a);
         this.upgrades = upgrades;
     }
 
