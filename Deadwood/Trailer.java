9e887f3a175ff421ead5db91c322b4ba558535a3
pleaseosaur <82362420+pleaseosaur@users.noreply.github.com>
2023-05-03 23:02:34 -0700

    minor modification to Trailer constructor

--- Deadwood/Trailer.java
+++ Deadwood/Trailer.java
@@ -1,8 +1,8 @@
 import java.util.List;
 
 public class Trailer extends Location{
-    public Trailer(String n, Scene s, List<Role> r, List<String> l, List<Take> takes) {
-        super(n, s, r, l, takes);
+    public Trailer(String n, Scene s, List<Role> r, List<String> l, Area a, List<Take> takes) {
+        super(n, s, r, l, takes, a);
     }
 
     public boolean actionAllowed(){
