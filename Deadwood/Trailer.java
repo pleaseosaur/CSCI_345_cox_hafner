0391036e62b1f2ccca40b1425ca598debeb3c67d
pleaseosaur <82362420+pleaseosaur@users.noreply.github.com>
2023-05-04 21:16:23 -0700

    comments

--- Deadwood/Trailer.java
+++ Deadwood/Trailer.java
@@ -1,12 +1,12 @@
 import java.util.List;
 
 public class Trailer extends Location{
+
     public Trailer(String name, List<String> neighbors, Area area) {
         super(name, neighbors, area);
-
     }
 
-    public boolean actionAllowed(){
+    public boolean actionAllowed(){ // --- probably don't need this now that Location is abstract
         // do logic here
         return false;
     }
