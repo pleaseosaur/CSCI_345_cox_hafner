aaeb85b890857706d4d9e15aeff4a448ceede690
Andrew Cox <coxa23@wwu.edu>
2023-05-06 16:54:52 -0700

    pseudo code for gameplay loop

--- Deadwood/Deadwood.java
+++ Deadwood/Deadwood.java
@@ -36,7 +36,18 @@ public class Deadwood {
     }
 
     public void runGame() {
-        // game loop
+        // while game is active (no. days > 0)
+            // while day is active (no. open Scenes > 1)
+                // while current player is active
+                    // display available actions (end turn should always be an option)
+                    // if player chooses move, display available locations
+                    // perform player action choice
+                    // display available actions again
+                    // if player chooses end turn, end the turn
+                    // if end turn is the only available option, end turn automatically
+                    // get next player
+                // when open scenes = 1, end the day
+            // when days = 0, end the game
     }
 
     public void move(Location location) {
