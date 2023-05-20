b29561ef70e817f468d55ec301ebc48f9a2a8eb8
Andrew Cox <coxa23@wwu.edu>
2023-05-20 16:37:04 -0700

    adding javafx libraries

--- Deadwood GUI/src/Main.java
+++ Deadwood GUI/src/Main.java
@@ -8,8 +8,15 @@
 import controller.Deadwood;
 import controller.GameManager;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main (String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) {
         GameManager manager = new GameManager();
         Deadwood game = new Deadwood(manager);
         game.startGame();
