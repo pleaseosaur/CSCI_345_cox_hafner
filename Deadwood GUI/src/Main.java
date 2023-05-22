2bfd3260ec0ffd65572c8fc5604e2ad8665d4000
pleaseosaur <82362420+pleaseosaur@users.noreply.github.com>
2023-05-21 19:15:00 -0700

    javafx still not working

--- Deadwood GUI/src/Main.java
+++ Deadwood GUI/src/Main.java
@@ -5,11 +5,11 @@
  */
 
 // imports
-import controller.Deadwood;
-import controller.GameManager;
 
+import controller.GameManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import view.DisplayGame;
 
 public class Main extends Application {
     public static void main (String[] args) {
@@ -18,7 +18,11 @@ public class Main extends Application {
 
     public void start(Stage primaryStage) {
         GameManager manager = new GameManager();
-        Deadwood game = new Deadwood(manager);
-        game.startGame();
+//        Deadwood game = new Deadwood(manager);
+        DisplayGame view = new DisplayGame();
+//        game.startGame();
+        primaryStage.setScene(view.getScene());
+        primaryStage.show();
+        view.startGame(manager);
     }
 }
--- Deadwood GUI/src/view/DisplayGame.java
+++ Deadwood GUI/src/view/DisplayGame.java
@@ -1,13 +1,45 @@
 package view;
 
+import controller.GameManager;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 
 public class DisplayGame {
     private ComboBox<Integer> playerSelect;
     private Button startButton;
     private Scene scene;
 
+    public DisplayGame() {
+        Image boardImage = new Image("/image/board.jpg");
+        ImageView boardView = new ImageView(boardImage);
+        StackPane root = new StackPane(boardView);
 
+        scene = new Scene(root, 1200, 900);
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+
+    public void startGame(GameManager manager) {
+        playerSelect = new ComboBox<>();
+        playerSelect.getItems().addAll(2, 3, 4, 5, 6, 7, 8);
+        playerSelect.setValue(2);
+        playerSelect.setLayoutX(500);
+        playerSelect.setLayoutY(500);
+
+        startButton = new Button("Start Game");
+        startButton.setLayoutX(500);
+        startButton.setLayoutY(550);
+        startButton.setOnAction(e -> {
+            manager.setupGame(playerSelect.getValue());
+        });
+
+        StackPane root = (StackPane) scene.getRoot();
+        root.getChildren().addAll(playerSelect, startButton);
+    }
 }
